3ae44f444a25307051169c09c4ba4ed2eea47932
jenny ma <jenny_lau_8@hotmail.com>
2020-04-25 21:53:58 -0400

    8.3

--- workshop/8-functions/8-exercise-3.js
+++ workshop/8-functions/8-exercise-3.js
@@ -3,7 +3,8 @@
 // The returned average should also be rounded to the nearest unit. (no decimals, please!)
 
 function calculateAverage(a, b, c, d, e) {
-  // Your code here
+  
+  return calculateAverage = (a + b + c + d + e)/5;
 }
 
 console.log(calculateAverage(76, 60, 83, 100, 78));
