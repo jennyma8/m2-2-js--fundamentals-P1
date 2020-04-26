ce3d9c7da17c62024bfe95017917784e6fa91ca4
jenny ma <jenny_lau_8@hotmail.com>
2020-04-26 17:39:48 -0400

    9.4

--- workshop/9-array-method-2/9-exercise-4.js
+++ workshop/9-array-method-2/9-exercise-4.js
@@ -11,12 +11,21 @@
 // Q4
 // Write a function that accepts a list of numbers and returns a new list with
 // all of the even numbers removed.
-function removeEvens(lst) {
-  // lst is an array of strings
+
+//Your goal is to add some stuff inside removeEvens, so that when you run this file with Node, it logs out the right value ([1, 3, 5, 7, 9, 11]). 
+//You're allowed to do whatever you want inside the body of that function, except you can't use for or while, you need to use forEach / map / filter / find / every
+//the methods we saw today
+
+function removeEvens(values) {
+  return values.filter(function (num) {
+    return num % 2 !== 0;
+  })
 }
+
 
 console.log(
   'Q4 removeEvens()',
   removeEvens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 );
 // should log [1, 3, 5, 7, 9, 11]
+
